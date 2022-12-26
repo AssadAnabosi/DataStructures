d36df63eb484df2879acb944e37c0c7d3906c1da
Assad Anabosi <assad.anabosi@icloud.com>
2022-12-27 01:59:30 +0200

    Update Queue_LinkedList.cpp

--- Queue_LinkedList.cpp
+++ Queue_LinkedList.cpp
@@ -2,112 +2,125 @@
 using namespace std;
 
 template <class N>
-class node{
-	public:
-	N data;
-	node <N>* next;
-		
-	node(){
-		next = NULL;
-	}
-	
-	node(N item){
-		data = item;
-		next = NULL;
-	}
-	node(N item, node <N>* next){
-		data = item;
-		this->next = next;
-	}
-	void print(){
-		if (next != NULL){
-			cout << data << "\t";
-			node<N>* p = next;
-			while (p != NULL){
-				cout << p->data << "\t";
-				p = p->next;
-			}
-			cout << endl;
-		}
-		else
-			cout << "Empty Node..!" << endl;
-	}
+class Node
+{
+public:
+    Node *next;
+    N value;
+
+    Node()
+    {
+        this->next = NULL;
+    }
+
+    Node(N value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+
+    Node(N value, Node<N> *next)
+    {
+        this->value = value;
+        this->next = next;
+    }
 };
 
 template <class Q>
-class queue{
-	node <Q>* front, *rear;
-	unsigned int currentSize;
-	public:
-	queue(){
-		currentSize = 0;
-		front = rear = NULL;
-	}
-	queue(queue<Q> &q){	//	copy const.
-		node <Q>*p =q.front;
-		while (p != NULL){
-			Append(p->data);
-			p=p->next;
-		}
-	}
-	bool empty(){
-		return (currentSize == 0);
-	}
-	bool Append(Q item){
-		node <Q>* tmp = new node <Q> (item);
-		if (tmp == NULL)
-			return false;
-		if (rear == NULL)
-			rear = front = tmp;
-		else{
-			rear->next = tmp->next;	//	double check
-			rear = tmp;
-		}
-		currentSize++;
-		return true;
-	}
-	bool full(){
-		node <Q>* tmp = new node <Q> ();
-		if (tmp == NULL)
-			return false;
-		return true;
-	}
-	bool serve(){
-		if(empty())
-			return false;
-		node <Q>* tmp = front;
-		front = front->next;
-		if (front == NULL)
-			rear = NULL;
-		currentSize--;
-		delete tmp;
-		return true;
-	}
-	bool retreive(Q &item){
-		if (empty())
-			return false;
-		item = front->data;
-		return true;
-	}
-	void operator=(queue <Q> &q){
-		while(serve());
-		if (q.empty())
-			return;
-		node <Q>*p =q.front;
-		while (p != NULL){
-			Append(p->data);
-			p=p->next;
-		}
-	}
-	~queue(){
-		while(serve());
-	}
+class Queue
+{
+    Node<Q> *front, *rear;
+    int count;
+
+    void clear()
+    {
+        while (this->Serve())
+            ;
+    }
+
+public:
+    Queue()
+    {
+        this->front = this->rear = NULL;
+        this->count = 0;
+    }
+
+    Queue(Queue<Q> &received)
+    {
+        this->front = this->rear = NULL;
+        this->count = 0;
+        this->operator=(received);
+    }
+
+    bool Empty()
+    {
+        return this->count == 0;
+    }
+
+    bool Append(Q item)
+    {
+        Node<Q> *temp = new Node<Q>(item);
+        // memory is Full
+        if (temp == NULL)
+            return false;
+        // adding the first node
+        else if (this->rear == NULL)
+            this->rear = this->front = temp;
+        // adding non-first node
+        else
+        {
+            this->rear->next = temp;
+            this->rear = temp;
+        }
+        this->count++;
+        return true;
+    }
+
+    bool Serve()
+    {
+        if (this->Empty())
+            return false;
+        Node<Q> *temp = this->front;
+        this->front = this->front->next;
+        // front reached end, then rear needs to be NULL
+        if (this->front == NULL)
+            this->rear = NULL;
+        delete temp;
+        this->count--;
+        return true;
+    }
+
+    bool Front(Q &item)
+    {
+        if (this->Empty())
+            return false;
+        item = this->front->value;
+        return true;
+    }
+
+    void operator=(Queue<Q> &received)
+    {
+        clear();
+        // if received queue is empty then they are the same
+        if (received.Empty())
+            return;
+        // we copy the received queue
+        Node<Q> *rptr = received.front; //  received tmp ptr
+        while (rptr != NULL)
+        {
+            this->Append(rptr->value);
+            rptr = rptr->next;
+        }
+    }
+
+    ~Queue()
+    {
+        clear();
+    }
 };
 
-int main(){
-// 	queue <int> q;
-// 	q.Append(1);
-// 	int item;
-// 	queue <int> q2;
-	return 0;
+int main()
+{
+    
+    return 0;
 }
